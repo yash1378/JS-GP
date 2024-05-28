6af44d9767876d0c9d2ad5ff177a922ab28cd7fd
yash1378 <slayycasm997@gmail.com>
2024-05-29 00:53:20 +0530

    slight changes

--- components/renroll/client.tsx
+++ components/renroll/client.tsx
@@ -230,7 +230,7 @@ function Enroll({ data, d }: EnrollmentProps) {
                   onClick={handleSubmit} // Call handleSubmit when "Yes, I'm sure" is clicked
                   className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-Damion-cursive rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
                 >
-                  Yes, I'm sure
+                  Yes, I&apos;m sure
                 </button>
                 <button
                   onClick={closeModal} // Close the modal when "No, cancel" is clicked
